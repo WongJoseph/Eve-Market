0343a03b368ef38f994f7e012df7f4b4078997ab
Kensen Alexandre <kalexandre13@gmail.com>
2017-12-01 18:15:45 -0500

    Added update user frontend

---
+++ EveFront/src/app/updateuser/updateuser.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from '../domain/user';
+import {AlertService} from '../service/alert.service';
+import {UserService} from '../service/user.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-updateuser',
+  templateUrl: './updateuser.component.html',
+  styleUrls: ['./updateuser.component.css']
+})
+export class UpdateuserComponent implements OnInit {
+  model: any = {};
+
+  constructor(private router: Router,
+              private userService: UserService,
+              private alertService: AlertService) {
+  }
+
+  ngOnInit() {
+    this.model.email = '';
+    this.model.password = '';
+    this.model.newpassword = '';
+  }
+
+  updateInfo() {
+    const user: User = this.model;
+    user.email = user.email.toLowerCase();
+    this.userService.update(user)
+      .subscribe(
+        data => {
+          // set success message and pass true paramater to persist the message after redirecting to the login page
+          this.alertService.success('Update successful', true);
+          this.router.navigate(['/search']);
+        },
+        error => {
+          this.alertService.error(error.error);
+        });
+  }
+
+}
