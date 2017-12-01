f98746684f5689b33b956a79e22e350f02188717
Kensen Alexandre <kalexandre13@gmail.com>
2017-11-30 22:45:45 -0500

    Added Registration Feature

--- EveFront/src/app/register/register.component.ts
+++ EveFront/src/app/register/register.component.ts
@@ -15,22 +15,22 @@ export class RegisterComponent {
   loading = false;
 
   constructor(private router: Router,
-              // private userService: UserService,
+              private userService: UserService,
               private alertService: AlertService) {
   }
 
   register() {
     this.loading = true;
-    // this.userService.create(this.model)
-    //   .subscribe(
-    //     data => {
-    //       // set success message and pass true paramater to persist the message after redirecting to the login page
-    //       this.alertService.success('Registration successful', true);
-    //       this.router.navigate(['/login']);
-    //     },
-    //     error => {
-    //       this.alertService.error(error);
-    //       this.loading = false;
-    //     });
+    this.userService.create(this.model)
+      .subscribe(
+        data => {
+          // set success message and pass true paramater to persist the message after redirecting to the login page
+          this.alertService.success('Registration successful', true);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          this.alertService.error(error.error);
+          this.loading = false;
+        });
   }
 }
--- evemarketback/src/main/java/com/ex/evemarketback/web/UserController.java
+++ evemarketback/src/main/java/com/ex/evemarketback/web/UserController.java
@@ -2,6 +2,7 @@ package com.ex.evemarketback.web;
 
 import com.ex.evemarketback.domain.User;
 import com.ex.evemarketback.service.UserService;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.ErrorMsg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
@@ -10,10 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.ServletContextAware;
 
 import javax.servlet.RequestDispatcher;
@@ -43,20 +41,31 @@ public class UserController {
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public void registerUser(@RequestParam Map<String, String> requestParams, HttpServletResponse response) throws IOException, ServletException {
+    @ResponseBody
+    public ResponseEntity<String> registerUser(@RequestBody Map<String, String> requestParams) throws IOException, ServletException {
         User user = new User();
         String error = null;
 
+        System.out.println(requestParams.get("username"));
+        System.out.println(requestParams.get("email"));
+        System.out.println(requestParams.get("password"));
         if (userService.findByusername(requestParams.get("username")) != null) {
             error = "A user with this Username already exists";
         }
         if (userService.findByemail(requestParams.get("email")) != null) {
             error = "A user with this Email Address already exists";
         }
-
+        if(requestParams.get("username") == ""){
+            error = "Username cannot be empty";
+        }
+        if(requestParams.get("email") == ""){
+            error = "Email Address cannot be empty";
+        }
+        if(requestParams.get("password") == ""){
+            error = "Password cannot be empty";
+        }
         if (error != null) {
-            response.sendRedirect("register.html");
-            response.getWriter().println("<font color=red>" + error + "</font>");
+            return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
 
         } else {
 
@@ -64,9 +73,10 @@ public class UserController {
             user.setEmail(requestParams.get("email"));
             user.setPassword(requestParams.get("password"));
             userService.save(user);
+            return new ResponseEntity(user, HttpStatus.OK);
 
 
         }
     }
-
 }
+
